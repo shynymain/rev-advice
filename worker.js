6477145a3194a462702d3e5d051cf8c3418a38e8
shynymain <umeparis0317@gmail.com>
2026-04-30 22:32:27 +0900

    Add files via upload

--- worker.js
+++ worker.js
@@ -1,17 +1,2 @@
-const headers = {
-  "content-type": "application/json;charset=utf-8",
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET,POST,OPTIONS",
-  "access-control-allow-headers": "content-type"
-};
-
-export default {
-  async fetch(req) {
-    if (req.method === "OPTIONS") return new Response("{}", { headers });
-    return new Response(JSON.stringify({
-      ok: true,
-      message: "フロント側ロジックで全自動予想します。",
-      mode: "local-prediction"
-    }), { headers });
-  }
-};
+const headers={"content-type":"application/json;charset=utf-8","access-control-allow-origin":"*","access-control-allow-methods":"GET,POST,OPTIONS","access-control-allow-headers":"content-type"};
+export default{async fetch(req){if(req.method==="OPTIONS")return new Response("{}",{headers});return new Response(JSON.stringify({ok:true,message:"予想はフロント側ローカルロジックで実行します。"}),{headers})}};
