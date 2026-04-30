89f08e36a1d00b380362c2f95cf542d28f5699e3
shynymain <umeparis0317@gmail.com>
2026-04-30 22:57:12 +0900

    Add files via upload

--- worker.js
+++ worker.js
@@ -1,2 +1,2 @@
 const headers={"content-type":"application/json;charset=utf-8","access-control-allow-origin":"*","access-control-allow-methods":"GET,POST,OPTIONS","access-control-allow-headers":"content-type"};
-export default{async fetch(req){if(req.method==="OPTIONS")return new Response("{}",{headers});return new Response(JSON.stringify({ok:true,message:"フロント側ロジックで全自動予想します。"}),{headers})}};
+export default{async fetch(req){if(req.method==="OPTIONS")return new Response("{}",{headers});return new Response(JSON.stringify({ok:true,message:"フロント側ロジックで予想します。"}),{headers})}};
